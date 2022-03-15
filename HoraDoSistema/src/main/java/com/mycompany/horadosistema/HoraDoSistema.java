596f230e77d70d6c561a7f4eeb030c639649f162
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-03-15 17:02:13 -0300

    Primeiros programas

--- HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
+++ HoraDoSistema/src/main/java/com/mycompany/horadosistema/HoraDoSistema.java
@@ -1,8 +1,12 @@
 package com.mycompany.horadosistema;
 
+import java.util.Date;
+
 public class HoraDoSistema {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
     }
 }
